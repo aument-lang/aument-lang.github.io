9d1003fb93023570db84ef22267b8cd88ad237ad
fermian <78496870+fermian@users.noreply.github.com>
2021-03-21 09:58:33 +0700

    Add TOC to tour & update tour

--- gen-tour.sh
+++ gen-tour.sh
@@ -31,10 +31,36 @@ src="<!doctype html>
 </div>
 
 <main class='container'>
+  <div class='row'>
+  <div class='col-4'>
+  <div id='toc-col'>
+  <h1 style='text-align:center;'>Table of contents</h1>
+  <div id='toc'>
+  <p>(Table of contents go here.)</p>
+  </div>
+  <noscript>
+  <p>Enable JavaScript.</p>
+  </noscript>
+  </div>
+  </div>
+  <div class='col-8' id='content'>
 $tour_cont
+  </div>
+  </div>
+  <footer class='bd-footer p-3 p-md-5 text-center'>
+  <div class='container'>
+    <ul class='bd-footer-links ps-0 mb-3'>
+      <li class='d-inline-block'><a href='https://github.com/aument-lang/aument'>GitHub</a></li>
+      <li class='d-inline-block ms-3'><a href='https://gitlab.com/aument-lang/aument'>GitLab (mirror)</a></li>
+    </ul>
+    <p class='mb-0'>Language is licensed under Apache 2.0, website licensed GPL-v3.</p>
+    <p class='mb-0'><small><em>&ndash; thank you to the Aument contributors &ndash;</em></small></p>
+  </div>
+</footer>
 </main>
 
 <script src='https://prismjs.com/prism.js'></script>
+<script src='../js/toc.js'></script>
 
 </body>
 </html>
